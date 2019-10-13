4bebeffb3bf8e8ca7dd35bb85f8ffecee684071b
Devansh <officialdevansh@gmail.com>
2019-10-13 22:57:47 +0530

    Updates

---
+++ precache-manifest.2b7c6441b6a10a342b7bdbfab845abae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c39806e37b45e052ee6964b90070afb2",
+    "url": "/BMS-Test/index.html"
+  },
+  {
+    "revision": "036cafb83f32b87bb45a",
+    "url": "/BMS-Test/static/css/main.e290324a.chunk.css"
+  },
+  {
+    "revision": "5e571bc0b7bf1b38fe17",
+    "url": "/BMS-Test/static/js/2.bd785fed.chunk.js"
+  },
+  {
+    "revision": "036cafb83f32b87bb45a",
+    "url": "/BMS-Test/static/js/main.45f14711.chunk.js"
+  },
+  {
+    "revision": "121e13efcda52603e34d",
+    "url": "/BMS-Test/static/js/runtime-main.d1bed88a.js"
+  }
+]);
